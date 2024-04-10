2b2349b4e34b711eef202888e310891f71f80848
RockArtist33 <jack-ogrady@outlook.com>
2024-04-10 23:28:04 +0100

    x

--- setup.py
+++ setup.py
@@ -4,8 +4,10 @@ from setuptools import find_packages, setup
 setup(
     name='justanotherlogger',
     packages=find_packages(include=['justanotherlogger']),
-    version='0.0.2',
+    version='0.0.3',
     description='A very simple logger',
+    long_description=open("README.md"),
+    long_description_content_type="text/markdown",
     author='RockArtist33',
     install_requires=[],
     setup_requires=['pytest-runner'],
